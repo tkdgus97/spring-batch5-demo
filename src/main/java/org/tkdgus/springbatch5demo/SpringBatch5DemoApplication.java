2e40fe2e13d3642e8285eb47228b624f73eb9bb4
tkdgus97 <breakking@naver.com>
2024-10-22 23:58:15 +0900

    feat: init batch project

---
+++ src/main/java/org/tkdgus/springbatch5demo/SpringBatch5DemoApplication.java
@@ -0,0 +1,13 @@
+package org.tkdgus.springbatch5demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch5DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatch5DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tkdgus/springbatch5demo/SpringBatch5DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tkdgus.springbatch5demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch5DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
